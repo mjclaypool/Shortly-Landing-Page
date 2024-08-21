18167f0c3f2d99da0d1ec365975653c13df8c567
mjclaypool <mallory.claypool@gmail.com>
2024-08-21 14:57:30 -0400

    Initial commit
    Set up Vite project with React, JavaScript, and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poppins": "Poppins",
+      },
+      colors: {
+        "primary-cyan": "hsl(180, 66%, 49%)",
+        "primary-dark-violet": "hsl(257, 27%, 26%)",
+        "secondary-red": "hsl(0, 87%, 67%)",
+        "neutral-grey": "hsl(0, 0%, 75%)",
+        "neutral-grey-violet": "hsl(257, 7%, 63%)",
+        "neutral-dark-blue": "hsl(255, 11%, 22%)",
+        "neutral-dark-violet": "hsl(260, 8%, 14%)",
+      }
+    },
+  },
+  plugins: [],
+}
+
