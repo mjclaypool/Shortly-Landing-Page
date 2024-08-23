790e1874894f4a203a07d30ad58bad1ea1f15e3d
mjclaypool <mallory.claypool@gmail.com>
2024-08-23 13:08:09 -0400

    Prep for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/Shortly-Landing-Page/",
   output: 'export', // Outputs a Single-Page Application (SPA).
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
+  images: {
+    unoptimized: true,
+  },
   async rewrites() {
     return [
       {
